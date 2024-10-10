a52076de93dd95af934595f17a74ca63f99d24ea
Thomas Dal Cin <tdalcin96@gmail.com>
2024-10-10 11:54:48 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       boxShadow: {
         'custom': '0px 0px 12px 0px rgba(30, 29, 29, 0.20)',
         'custom-hover': ' 0px 0px 32px 0px rgba(38, 38, 38, 0.32)',
+      },
+      backgroundSize:{
+        '55%': '55%',
       }
     },
   },
